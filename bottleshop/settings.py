cdca947b66fc4b0682e286399ebdb19f6ec4e68e
felygit <felixwandera055@gmail.com>
2023-11-08 19:42:45 +0300

    Updated files

--- bottleshop/settings.py
+++ bottleshop/settings.py
@@ -94,12 +94,12 @@ WSGI_APPLICATION = 'bottleshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-#DATABASES = {
- #   'default': {
-  #      'ENGINE': 'django.db.backends.sqlite3',
-   #     'NAME': BASE_DIR / 'db.sqlite3',
-    #}
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 #actual
 #DATABASES = {
@@ -113,16 +113,16 @@ WSGI_APPLICATION = 'bottleshop.wsgi.application'
     #}
 #}
 
-DATABASES = {
-    'default': {
-        'ENGINE': os.environ.get("DB_ENGINE"),
-        'NAME': os.environ.get("DB_NAME"),
-        'USER': os.environ.get("DB_USER"),
-        'PASSWORD': os.environ.get("DB_PASSWORD"),
-        'PORT': os.environ.get("DB_PORT"),
-        'HOST': os.environ.get("DB_HOST")
-    }
-}
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': os.environ.get("DB_ENGINE"),
+   #     'NAME': os.environ.get("DB_NAME"),
+    #    'USER': os.environ.get("DB_USER"),
+     #   'PASSWORD': os.environ.get("DB_PASSWORD"),
+      #  'PORT': os.environ.get("DB_PORT"),
+       # 'HOST': os.environ.get("DB_HOST")
+    #}
+#}
 
 
 
