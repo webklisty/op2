7658a3cf26661bcaf4a7b2048a750194dc6a7784
felygit <felixwandera055@gmail.com>
2023-11-27 14:53:35 +0300

    e

--- bottleshop/settings.py
+++ bottleshop/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # Disable debug mode in production
-#DEBUG=True
+DEBUG=True
 
 # Set the logging level to INFO in production
 
